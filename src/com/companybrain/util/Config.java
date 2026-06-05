0e00f887774fa55d68e6105a8c5605721082b024
@Ali.Muzaffar <muzaffarali3633@gmail.com>
2026-06-06 00:34:46 +0500

    Store DB in APPDATA; update Maven build

--- src/com/companybrain/util/Config.java
+++ src/com/companybrain/util/Config.java
@@ -2,12 +2,23 @@ package com.companybrain.util;
 
 import java.io.File;
 
-
 public class Config {
+
+    private static final String APP_FOLDER = "CompanyBrain";
     private static String dbName = "company_brain.db";
 
     public static String getDbUrl() {
-        return "jdbc:sqlite:" + dbName;
+
+        String appDataPath =System.getenv("APPDATA")+ File.separator+ APP_FOLDER;
+
+        File directory = new File(appDataPath);
+
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        String dbPath =appDataPath+ File.separator+ dbName;
+        return "jdbc:sqlite:" + dbPath;
     }
 
     public static void setDbName(String name) {
@@ -15,7 +26,10 @@ public class Config {
     }
 
     public static boolean dbFileExists() {
-        File file = new File(dbName);
-        return file.exists();
+
+        String appDataPath =System.getenv("APPDATA")+ File.separator+ APP_FOLDER;
+        String dbPath =appDataPath+ File.separator+ dbName;
+
+        return new File(dbPath).exists();
     }
 }
